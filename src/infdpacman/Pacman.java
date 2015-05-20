b1a37b69f22ef462c82b5984bc0d4f29c55e1e17
vejbye <ericvejbye@gmail.com>
2015-05-20 12:16:12 +0200

    fisrt commit

---
+++ src/infdpacman/Pacman.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package infdpacman;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Pacman extends Poppetje {
+    public int leven;
+    public int score;
+    
+    
+    
+}
---
+++ src/infdpacman/Speelbord.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package infdpacman;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Speelbord {
+    int height;
+    int length;
+}
