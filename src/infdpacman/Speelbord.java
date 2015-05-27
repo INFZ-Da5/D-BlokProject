4db234f5b6097c84af6488acfd08951238e7c2de
vejbye <ericvejbye@gmail.com>
2015-05-27 12:33:27 +0200

    Speelbord voorzien van vakjes
    vakjes hebben nog geen inhoud

--- src/infdpacman/Speelbord.java
+++ src/infdpacman/Speelbord.java
@@ -9,4 +9,11 @@ import javax.swing.JPanel;
 public class Speelbord extends JPanel {
     int height;
     int length;
+    Vakje[][] vakjes;
+    
+    
+    
+    public Speelbord(){
+    vakjes = new Vakje[3][3]; 
+    }
 }
