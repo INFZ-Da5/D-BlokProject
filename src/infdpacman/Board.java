a3ef9d65f7bd2edafef530953579863e456c04b3
vejbye <ericvejbye@gmail.com>
2015-06-07 14:30:55 +0200

    level1 gevuld?

--- src/infdpacman/Board.java
+++ src/infdpacman/Board.java
@@ -20,6 +20,7 @@ public abstract class Board extends JPanel  {
     Pacman p = new Pacman(this);
     
     public Board(){
+        initLevel();
         this.requestFocusInWindow();
         this.addKeyListener(p);
         this.repaint();
--- src/infdpacman/Level1.java
+++ src/infdpacman/Level1.java
@@ -1,5 +1,11 @@
 package infdpacman;
 
+import java.util.LinkedList;
+import infdpacman.character.Pacman;
+import infdpacman.character.Ghost;
+import infdpacman.character.DrunkGhost;
+import infdpacman.item.*;
+
 /**
  *
  * @author CVD
@@ -15,7 +21,7 @@ class Level1 extends Board {
         5 = superpill
         6 = banana
     */
-    int [][] grid = new int[][]{
+    Object [][] grid = new Object[][]{
         { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
         { 0, 4, 4, 4, 0, 4, 4, 4, 0 },
         { 0, 5, 4, 4, 3, 4, 4, 5, 0 },
@@ -34,12 +40,37 @@ class Level1 extends Board {
 
     @Override
     public void initLevel() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        System.out.println("test");
     }
 
     @Override
     public void fillVakjes() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+               for (int row = 0; row < height; row++) {
+            for (int col = 0; col < length; col++) {
+                if((int)grid[row][col] == 0){
+                grid[row][col] = new Wall();}
+                
+                if((int)grid[row][col] > 0){
+                    LinkedList inhoud = new LinkedList();
+                    EmptyCell cell = new EmptyCell();
+                grid[row][col] = cell;
+                cell.setInhoud(inhoud);
+                
+                switch((int)grid[row][col]){
+                    case 1: inhoud.add(new Pacman(this));
+                    case 2: inhoud.add(new Ghost());
+                    case 3: inhoud.add(new DrunkGhost());
+                    case 4: inhoud.add(new Pill());
+                    case 5: inhoud.add(new SuperPill());
+                    case 6: inhoud.add(new Banana());
+                
+                }
+               
+                
+                
+            }
+                
+            }
     }
-    
+    } 
 }
