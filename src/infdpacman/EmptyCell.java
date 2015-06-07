9c4f379b5068821e54cecf2494997c57ee44bae1
CVD <cornelisvdam@hotmail.com>
2015-06-07 14:52:57 +0200

    remove duplicate code @ emptycell

--- src/infdpacman/EmptyCell.java
+++ src/infdpacman/EmptyCell.java
@@ -2,9 +2,9 @@ package infdpacman;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -12,34 +12,21 @@ import java.util.Map;
  * @author Lenovo
  */
 public class EmptyCell extends Cell{
-    private LinkedList inhoud = new LinkedList(); 
-    private Map<EmptyCell, Direction> neighbors = new HashMap<>();
-    private ArrayList<GameElement> gameElements = new ArrayList();
+    private List<GameElement> inhoud = new LinkedList<GameElement>(); 
+    private Map<Direction, EmptyCell> neighbors = new HashMap<>();
     
     public EmptyCell(){
         this.setBackground(Color.BLACK);
     }
     
-    //draw the object, not the arraylist --> cant with typecasting
     @Override
     public void draw(Graphics g){
-        for(GameElement elem: gameElements){
-            elem.draw(g,this.getWidth(),this.getHeight());
+        for (GameElement inhoud1 : inhoud) {
+            inhoud1.draw(g, this.getWidth(), this.getHeight());
         }
     }
     
-    public LinkedList getInhoud() {
-        return inhoud;
-    }
-
     public void setInhoud(LinkedList inhoud) {
         this.inhoud = inhoud;
     } 
-    
-    public void vulVakje(){
-        gameElements.clear();
-        for(Object obj : inhoud){
-            gameElements.add((GameElement)obj);
-        }
-    }
 }
