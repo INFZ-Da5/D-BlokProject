12a1e897a32ecdefebf4da0b05e73c5deba25cd2
vejbye <ericvejbye@gmail.com>
2015-06-07 15:35:07 +0200

    extra array aangemaakt

--- src/infdpacman/Level1.java
+++ src/infdpacman/Level1.java
@@ -1,11 +1,12 @@
 package infdpacman;
 
-import java.util.LinkedList;
-import infdpacman.character.Pacman;
-import infdpacman.character.Ghost;
 import infdpacman.character.DrunkGhost;
+import infdpacman.character.Ghost;
+import infdpacman.character.Pacman;
 import infdpacman.item.*;
 import java.awt.GridLayout;
+import java.util.LinkedList;
+
 
 /**
  *
@@ -22,7 +23,7 @@ class Level1 extends Board {
         5 = superpill
         6 = banana
     */
-    Object [][] grid = new Object[][]{
+    int [][] grid = new int[][] {
         { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
         { 0, 4, 4, 4, 0, 4, 4, 4, 0 },
         { 0, 5, 4, 4, 3, 4, 4, 5, 0 },
@@ -50,18 +51,20 @@ class Level1 extends Board {
 
     @Override
     public void fillVakjes() {
+        Cell[][] cellgrid = new Cell[width][height];
+        
         for (int row = 0; row < height; row++) {
             for (int col = 0; col < width; col++) {
-                if((int)grid[row][col] == 0){
-                grid[row][col] = new Wall();}
+                if(grid[row][col] == 0){
+                cellgrid[row][col] = new Wall();}
                 
-                if((int)grid[row][col] > 0){
+                if(grid[row][col] != 0){
                     LinkedList inhoud = new LinkedList();
                     EmptyCell cell = new EmptyCell();
-                    grid[row][col] = cell;
+                    cellgrid[row][col] = cell;
                     cell.setInhoud(inhoud);
 
-                    switch((int)grid[row][col]){
+                  /*  switch(grid[row][col]){
                         case 1: inhoud.add(new Pacman(this));
                         case 2: inhoud.add(new Ghost());
                         case 3: inhoud.add(new DrunkGhost());
@@ -69,7 +72,7 @@ class Level1 extends Board {
                         case 5: inhoud.add(new SuperPill());
                         case 6: inhoud.add(new Banana());
 
-                    }
+                    }*/
                 }
             }
         }
