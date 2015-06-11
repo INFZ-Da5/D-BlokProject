e5fe6b7d4ca361d7887eb04c23ad35492620c537
CVD <cornelisvdam@hotmail.com>
2015-06-11 14:03:05 +0200

    winner big letters

--- src/infdpacman/view/Winner.java
+++ src/infdpacman/view/Winner.java
@@ -1,5 +1,10 @@
 package infdpacman.view;
 
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.font.TextLayout;
+import java.awt.geom.Rectangle2D;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -10,8 +15,13 @@ import javax.swing.JPanel;
 public class Winner extends JPanel {
         
         public Winner(){
-            JLabel label = new JLabel("You win!");
-            label.setSize(this.getWidth(), this.getHeight());
-            this.add(label);
+            JLabel winLabel = new JLabel("You win!");
+            winLabel.setFont(new Font("Helvetica", Font.PLAIN, 88));
+            FontMetrics fm = winLabel.getFontMetrics(winLabel.getFont());
+            TextLayout layout = new TextLayout(winLabel.getText(), winLabel.getFont(), fm.getFontRenderContext());
+            Rectangle2D bounds = layout.getBounds();
+            Dimension d = winLabel.getPreferredSize();
+            winLabel.setSize(this.getWidth(), this.getHeight());
+            this.add(winLabel);
         }
 }
