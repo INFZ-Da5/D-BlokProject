7430ee0b149d4a2e1453835840620fb9f6c41f5a
vejbye <ericvejbye@gmail.com>
2015-06-16 15:34:25 +0200

    high score in menu

--- src/infdpacman/Menu.java
+++ src/infdpacman/Menu.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 /**
@@ -15,10 +16,11 @@ import javax.swing.JPanel;
  */
 public class Menu implements ActionListener {
 public JFrame menu;
+public Game game;
     
     
 public void init(){
-
+   
    menu = new JFrame();
    menu.setLayout(new GridLayout(3,0));
    menu.setSize(200, 200);
@@ -28,6 +30,9 @@ public void init(){
         JButton settings = new JButton("settings");
             settings.setActionCommand(Actions.SETTINGS.name());
             settings.addActionListener(this);
+        JButton Highscore= new JButton("Highscores");
+            Highscore.setActionCommand(Actions.SCORE.name());
+            Highscore.addActionListener(this);
         JButton quit= new JButton("quit");
             quit.setActionCommand(Actions.QUIT.name());
             quit.addActionListener(this);
@@ -35,6 +40,7 @@ public void init(){
 
             menu.add(start);
             menu.add(settings);
+            menu.add(Highscore);
             menu.add(quit);
            
         menu.setVisible(true);
@@ -43,10 +49,20 @@ public void init(){
     @Override
     public void actionPerformed(ActionEvent e) {
 if (e.getActionCommand().equals(Actions.START.name())) {
-        Game game = new Game();
+        game = new Game();
         game.initFrame();
         menu.dispose();
-        }else if(e.getActionCommand().equals(Actions.SETTINGS.name())){            
+        }else if(e.getActionCommand().equals(Actions.SETTINGS.name())){  
+            
+        }else if(e.getActionCommand().equals(Actions.SCORE.name())){ 
+             String message = "Highscores:\n";
+            if(!game.hscores.isEmpty()){
+                for(String score: game.hscores){
+                message = message + "" + score + "\n";
+                }
+            }
+           JOptionPane.showMessageDialog(null, message, "scorelist", JOptionPane.ERROR_MESSAGE);  
+            
 
         } else if(e.getActionCommand().equals(Actions.QUIT.name())){
            menu.dispose();
